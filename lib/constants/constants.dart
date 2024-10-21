235e1e07166fb40b02c1fc46eba038ec8c084d92
MartinaMabiloft <martina@mabiloft.com>
2024-10-21 11:17:03 +0200

    Refactor file paths in .gitignore and remove unused code
    - Update file paths in .gitignore to match the new directory structure
    - Remove unused code files: app_colors.dart, constants.dart, and text_styles.dart

--- lib/constants/constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/foundation.dart' show immutable;
-
-@immutable
-class Constants {
-  static const String apiKey = '5fa7f60a0e7bbcebe17456b45798a46f';
-  static const String apiKey2 = '19d2fd7aab958bfe7f61000e1f5151cc';
-}
